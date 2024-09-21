8760d824702bc508d92862f6ff44582e454e5715
OohBen <89159600+OohBen@users.noreply.github.com>
2024-09-20 23:25:16 -0400

    pls work

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import webvtt
+# Flask app
 app = Flask(__name__)
+@app.route("/get", methods=["GET"])
+def file_get():
+    url = request.args.get('url')
+    resp = requests.get(url)
+    vtt = webvtt.from_string(resp.text)
+    # Return the content in json format
+    # get only the text from the vtt file in one long string
+    data = {'text': ' '.join([c.text for c in vtt.captions])}['text']
+    return str(data)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
-
-if __name__ == "__main__":
-    app.run()
